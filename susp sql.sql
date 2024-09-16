fd268464ea2e34ce5783cd17583cc917ecb35609
Mohammed0192 <101655915+Mohammed0192@users.noreply.github.com>
2024-09-16 12:53:08 +0100

    Update susp sql.sql

--- susp sql.sql
+++ susp sql.sql
@@ -19,3 +19,17 @@ GROUP BY
     e.characteristic
 ORDER BY UK_COVID_Susp desc
 LIMIT 10
+
+
+
+SELECT c.region_name,
+SUM(CASE WHEN c.characteristic LIKE 'FSM - Eligible' AND c.time_period < 201920 THEN perm_excl ELSE 0 END) - SUM(CASE WHEN c.characteristic LIKE 'FSM - Not eligible' AND c.time_period < 201920 THEN perm_excl ELSE 0 END) AS Pre_COVID_FSM_Diff,
+SUM(CASE WHEN c.characteristic LIKE 'FSM - Eligible' AND v.time_period > 201920 THEN perm_excl ELSE 0 END) - SUM(CASE WHEN c.characteristic LIKE 'FSM - Not eligible' AND c.time_period > 201920 THEN perm_excl ELSE 0 END) AS Post_COVID_FSM_Diff,
+ROUND(AVG(CASE WHEN v.time_period < 201920 THEN v.rate ELSE 0 END),1) AS PreCOVID_Rate,
+ROUND(AVG(CASE WHEN v.time_period > 201920 THEN v.rate ELSE 0 END), 1) AS PostCOVID_Rate
+FROM characteristics_excl2 AS c
+INNER JOIN school_vacancy_data AS v
+ON c.region_name = v.region_name
+GROUP BY c.region_name
+ORDER BY Post_COVID_FSM_Diff DESC
+LIMIT 15
